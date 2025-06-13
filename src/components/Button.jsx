ba9915e9930dd5d62169a3a3f48057bcb2b41b27
VipeR-69 <computershubham7@gmail.com>
2025-06-13 18:20:58 +0530

    Added an animated counter for stats and scroll button setup

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -9,7 +9,7 @@ const Button = ({ text, className, id }) => {
         if(target && id){
           const offest = window.innerHeight * 0.15;
           const top = target.getBoundingClientRect().top + window.scrollY - offest;
-          window.scrollTo( { top, behavior: 'smooth' })
+          window.scrollTo( { top, behavior: "smooth" })
         }
       }}
       className={`${className ?? ''} cta-wrapper`}
